71b211a1c371b5f8fd0b0a93b2ad42d683b769ca
hypothe <markeagle98@gmail.com>
2021-08-19 11:35:23 +0200

    <Add> enum AS result definition

--- src/sciroc_darknet_bridge_node.cpp
+++ src/sciroc_darknet_bridge_node.cpp
@@ -12,10 +12,28 @@ class EnumBridge : public EnumAS
 {
 	public:
 		EnumBridge(ros::NodeHandle nh_, std::string action_server_name)
-		: EnumAS(nh_, action_server_name){}
+		: EnumAS(nh_, action_server_name),
+			detThreshold_(0)
+			{
+				node_handle_.param("objdet/specs/detection_threshold", detThreshold_, float(0));
+			}
 	private:
-		void resultCB(){}
-		void saveGoalData(){}
+		void saveGoalDataImp(){}
+		void setResultImp()
+		{
+			float foundBoxes = 0;
+			for (auto imageBoxes : detectedBoxes)
+			{
+				for (auto box : imageBoxes.bounding_boxes)
+				{
+					if (box.probability >= detThreshold_)	foundBoxes++;
+				}
+			}
+			foundBoxes /= detectedBoxes.size();
+			action_.action_result.result.n_found_tags = std::round(foundBoxes);
+		}
+
+		float detThreshold_;
 };
 
 int main(int argc, char** argv) {
