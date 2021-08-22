72cde1f6b9cb5fe311b421f4db390c1c9780816d
hypothe <markeagle98@gmail.com>
2021-08-22 19:32:39 +0200

    <Add> action servers' names from param server

--- src/sciroc_darknet_bridge_node.cpp
+++ src/sciroc_darknet_bridge_node.cpp
@@ -131,9 +131,13 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "sciroc_darknet_bridge_node");
   ros::NodeHandle nodeHandle;
 	// TODO: get action names from rosparam
-  EnumBridge enum_bridge(nodeHandle, "enum_bridge_as");
-  ClasBridge clas_bridge(nodeHandle, "clas_bridge_as");
-  CompBridge comp_bridge(nodeHandle, "comp_bridge_as");
+	std::string enum_name, clas_name, comp_name;
+	nodeHandle.param("objdet/actions/enumeration/topic", enum_name, std::string("/enum_bridge_as"));
+	nodeHandle.param("objdet/actions/classification/topic", clas_name, std::string("/clas_bridge_as"));
+	nodeHandle.param("objdet/actions/comparison/topic", comp_name, std::string("/comp_bridge_as"));
+	EnumBridge enum_bridge(nodeHandle, enum_name);
+	ClasBridge clas_bridge(nodeHandle, clas_name);
+  CompBridge comp_bridge(nodeHandle, comp_name);
 
   ros::spin();
   return 0;
